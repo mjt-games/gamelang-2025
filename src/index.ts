9aa698660de48eb9f5f367c3aee8165d90ee820e
Matt Taylor <matt@matthewjosephtaylor.com>
2025-04-10 10:34:33 -0500

    exported parts of spec

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { createGameLang } from "./lang/createGameLang";
 import { createState } from "./runtime/state/State";
 
+export * from "./lang/spec/GameLangSpec";
+
+export * from "./lang/spec/Value";
+export * from "./lang/spec/SourceIndex";
+export * from "./lang/spec/SourceLocation";
+export * from "./lang/spec/Op";
+
 export const GameLangs = {
   createGameLang,
   createState,
